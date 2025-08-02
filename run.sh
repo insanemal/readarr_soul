bc35f026b6c648fe9edd9ee318d14d7ce01ae97e
Malcolm Haak <insanemal@gmail.com>
2025-08-02 13:50:31 +1000

    More rename work. Prep for Docker container push to DockerHub

--- run.sh
+++ run.sh
@@ -7,11 +7,11 @@
 INTERVAL=${SCRIPT_INTERVAL:-300}
 
 while true; do
-    if ps aux | grep "[s]oularr.py" > /dev/null; then
+    if ps aux | grep "rsoul.py" > /dev/null; then
         echo "Soularr is already running. Exiting..."
     else
         #Pass in the arguments given to the bash script over to the Python script
-        python -u /app/readarr_soul.py "$@"
+        python -u /app/rsoul.py "$@"
     fi
 
     dt=$(date '+%d/%m/%Y %H:%M:%S');
